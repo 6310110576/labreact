1357b82c4cc3147e1382cd4040742418d2655326
6310110576 <6310110576@psu.ac.th>
2022-08-20 21:50:18 +0700

    part7.1

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useRef } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 
 export default function CharacterCard(props) {
     const [active, setActive] = useState(false);
@@ -6,18 +6,18 @@ export default function CharacterCard(props) {
     const activate = () => {
         if(!active){
             setActive(true)
-            props.activationHandler(props.value)    
+            props.activationHandler(props.value)
         }
+    }
     useEffect(() => {
         if(attemptRef.current != props.attempt){
             setActive(false);
             attemptRef.current = props.attempt
         }
     })
-        
- }
  const className = `card ${active ? 'activeCard': ''}`
- 
+
+
  return (
     <div className={className} onClick={activate}>{props.value}</div>
  )
